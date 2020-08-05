f45f5a744e4c28831a68b75816b22c546d3745c9
CAM603 <cameronhawley@comcast.net>
2020-08-05 11:52:54 -0400

    add footer component

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,24 @@
+// Styles
+import styled from "@emotion/styled";
+// Components
 import Header from "../Header";
+import Footer from "../Footer";
 
 export default function Layout(props) {
     return (
         <div>
             <Header />
-            <main>{props.children}</main>
+            <MainStyled>{props.children}</MainStyled>
+            <Footer />
         </div>
     );
 }
+
+const MainStyled = styled.main`
+    min-height: 90vh;
+    padding: 0 0.5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
