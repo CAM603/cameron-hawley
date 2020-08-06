af824535d4f1bf4218934ec5e14ffe355d8e7fc8
CAM603 <cameronhawley@comcast.net>
2020-08-06 14:40:01 -0400

    add projects section

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,18 @@
+import styled from "@emotion/styled";
+
 export default function Footer() {
     return (
-        <div>
-            <p>Footer</p>
-        </div>
+        <FooterStyled>
+            <p>Created with Next.js by Cameron Hawley</p>
+        </FooterStyled>
     );
 }
+
+const FooterStyled = styled.footer`
+    background-color: ${(props) => props.theme.colors.primary};
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    margin-top: 30px;
+    padding: 60px 30px;
+`;
