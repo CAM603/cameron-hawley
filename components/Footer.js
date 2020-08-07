720d0db56020ee8845b96ed79dac22b085eff3ec
CAM603 <cameronhawley@comcast.net>
2020-08-07 08:25:37 -0400

    add images and icons

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,12 @@ export default function Footer() {
     return (
         <FooterStyled>
             <p>Created with Next.js by Cameron Hawley</p>
+            <p>
+                Icons by{" "}
+                <a href="https://icons8.com/" target="_blank" rel="external">
+                    icons8
+                </a>
+            </p>
         </FooterStyled>
     );
 }
@@ -12,7 +18,13 @@ const FooterStyled = styled.footer`
     background-color: ${(props) => props.theme.colors.primary};
     padding: 20px;
     display: flex;
+    flex-direction: column;
     justify-content: center;
+    align-items: center;
     margin-top: 30px;
     padding: 60px 30px;
+
+    p {
+        line-height: 1.5;
+    }
 `;
--- components/icons.js
+++ components/icons.js
@@ -6,15 +6,24 @@ const ImageStyled = styled.img`
 `;
 
 const icons = [
-    { image: <ImageStyled src="/images/html.png" alt="html5" />, id: 1 },
-    { image: <ImageStyled src="/images/css.png" alt="css" />, id: 2 },
+    { icon: <ImageStyled src="/images/html.png" alt="html5" />, id: 1 },
+    { icon: <ImageStyled src="/images/css.png" alt="css" />, id: 2 },
     {
-        image: <ImageStyled src="/images/javascript.png" alt="javascript" />,
+        icon: <ImageStyled src="/images/javascript.png" alt="javascript" />,
         id: 3,
     },
-    { image: <ImageStyled src="/images/python.png" alt="python" />, id: 4 },
-    { image: <ImageStyled src="/images/react.png" alt="react" />, id: 5 },
-    { image: <ImageStyled src="/images/node.png" alt="node" />, id: 6 },
+    { icon: <ImageStyled src="/images/python.png" alt="python" />, id: 4 },
+    { icon: <ImageStyled src="/images/react.png" alt="react" />, id: 5 },
+    { icon: <ImageStyled src="/images/node.png" alt="node" />, id: 6 },
 ];
 
-export default icons;
+const socials = [
+    { icon: <ImageStyled src="/images/email.png" alt="email" />, id: 1 },
+    { icon: <ImageStyled src="/images/github.png" alt="github" />, id: 2 },
+    {
+        icon: <ImageStyled src="/images/linkedin.png" alt="linkedin" />,
+        id: 3,
+    },
+];
+
+export { icons, socials };
