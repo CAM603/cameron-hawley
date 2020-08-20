699fbde00bd048310f60d43541f549262aeba0f1
CAM603 <cameronhawley@comcast.net>
2020-08-20 19:46:54 -0400

    fix broken open graph

--- next-seo.config.js
+++ next-seo.config.js
@@ -4,8 +4,16 @@ export default {
     openGraph: {
         type: "website",
         locale: "en_IE",
-        url: "https://www.camhawley.com",
+        url: "https://camhawley.com/",
         site_name: "Cameron Hawley",
+        images: [
+            {
+                url: "https://camhawley.com/images/favicon-32x32.png",
+                width: 32,
+                height: 32,
+                alt: "Cameron Hawley",
+            },
+        ],
     },
     twitter: {
         handle: "@none",
